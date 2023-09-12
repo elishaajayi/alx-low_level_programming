bfb82f959b977ec4b563e3ca5ac57ca98dea4beb
elishaajayi <ajayienterprises.dev@gmail.com>
2023-09-12 14:06:58 -0700

    Normal complete

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* add - literally adds two integers
+* @x: first integer
+* @y: second integer
+* Return: the additive value of x and y
+*/
+int add(int x, int y)
+{
+	return (x + y);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,3 +7,4 @@ int print_sign(int n);
 int _abs(int n);
 void jack_bauer(void);
 void times_table(void);
+int add(int x, int y);
