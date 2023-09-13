59b565919e925fbedc191f2f727235ad112835a0
elishaajayi <ajayienterprises.dev@gmail.com>
2023-09-12 21:57:13 -0700

    Hot fix 3

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,49 +1,49 @@
 #include "main.h"
 
 /**
-* print_times_table - as the name implies
-* @n: the numbe of times to multiply
-* Return: void as in none
-*/
+ * print_times_table - Print the `n` times table, starting with 0.
+ * Description: If `n` is greater than 15 or less than 0, print nothing.
+ * @n: int type number
+ */
 void print_times_table(int n)
 {
-	int width, height, value;
+	int x = 0, y, z;
 
-	if (!(n > 15 || n < 0))
+	if (n > 15 || n < 0)
+		return;
+	while (x <= n)
 	{
-		for (height = 0; height <= n; height++)
+		for (y = 0; y <= n; y++)
 		{
-			for (width = 0; width <= n; width++)
+			z = x * y;
+			if (z > 99)
 			{
-				value = height * width;
-
-				if (width != 0 && value < 10)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-				}
-				if (width != 0 && value > 9 && value < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
-				if (width != 0 && value > 99)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-				if (value < 10)
-					_putchar(value + '0');
-				else
-				{
-					_putchar((value / 10) + '0');
-					_putchar((value % 10) + '0');
-				}
+				_putchar(z / 100 + '0');
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
 			}
-			_putchar('\n');
+			else if (z > 9)
+			{
+				_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+				_putchar(z + '0');
+
+			if (y != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
+		_putchar('\n');
+		x++;
 	}
 }
