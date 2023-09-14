47781c5619e8b749254c4461dd5817a800f06f6f
elishaajayi <ajayienterprises.dev@gmail.com>
2023-09-14 13:54:15 -0700

    I tried

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* print_number - print any number with putchar
+* @n: the number to print
+* Return: void as in none
+*/
+void print_number(int n)
+{
+	int rem;
+
+	if (n <
