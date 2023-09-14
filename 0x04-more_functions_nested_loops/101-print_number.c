7f9bf56b57e4c9e8e98b055eed3206ab61c69bd9
elishaajayi <ajayienterprises.dev@gmail.com>
2023-09-14 14:06:51 -0700

    Test

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,12 +1,22 @@
 #include "main.h"
 
 /**
-* print_number - print any number with putchar
-* @n: the number to print
-* Return: void as in none
-*/
+ * print_number - print any number with putchar
+ * @n: the number to print
+ * Return: void as in none
+ */
 void print_number(int n)
 {
-	int rem;
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
 
-	if (n <
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+
+	_putchar((n % 10) + '0');
+}
