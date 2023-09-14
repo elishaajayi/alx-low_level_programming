43c49a3b4ab74d51d333a75018e2f75473734d3f
elishaajayi <ajayienterprises.dev@gmail.com>
2023-09-14 10:21:32 -0700

    testing main.h and putchar:

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _isupper - function to check uppercase or lowercase
+* @c: ascii value of the parameter
+* Return: 1 for uppercase and 0 for anything else
+*/
+int _isupper(int c)
+{
+	if (c >= 'A')
+		return (1);
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+More functions and nested loops <br>
+10 tasks: mandatory <br>
+2 tasks: advanced <br>
