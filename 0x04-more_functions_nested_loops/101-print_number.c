67e889264e229caaa9eeacf9ce4e126dcbc1fc06
elishaajayi <ajayienterprises.dev@gmail.com>
2023-09-14 21:59:20 -0700

    Try

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include <limits.h>
+
+void limits(int n);
 
 /**
  * print_number - print any number with putchar
@@ -7,16 +10,67 @@
  */
 void print_number(int n)
 {
+	int x = n;
+	int digit;
+	int places = 1000000000;
+
 	if (n < 0)
 	{
-		_putchar(45);
-		n = -n;
+		x = -n;
+		_putchar('-');
 	}
 
-	if (n / 10 != 0)
+	limits(n);
+
+	if (n == 0)/* A */
+		_putchar('0');
+	else
 	{
-		print_number(n / 10);
+		while (places > x)/* B */
+			places /= 10;
+		while (places > 0)
+		{
+			digit = x / places;/* C */
+			_putchar((digit % 10) + '0');
+			places /= 10;
+		}
 	}
+}
 
-	_putchar((n % 10) + '0');
+void limits(int n)
+{
+	if (n == INT_MAX || n == INT_MIN)
+	{
+	while (1)
+	{
+	if (n == INT_MAX)
+	{
+		_putchar('2');
+		_putchar('1');
+		_putchar('4');
+		_putchar('7');
+		_putchar('4');
+		_putchar('8');
+		_putchar('3');
+		_putchar('6');
+		_putchar('4');
+		_putchar('7');
+		break;
+	}
+	else if (n == INT_MIN)
+	{
+		_putchar('2');
+		_putchar('1');
+		_putchar('4');
+		_putchar('7');
+		_putchar('4');
+		_putchar('8');
+		_putchar('3');
+		_putchar('6');
+		_putchar('4');
+		_putchar('8');
+		break;
+	}
+	}
+	}
 }
