678b3c4b2fad09fad738c037050ed05a1cb3ba79
elishaajayi <ajayienterprises.dev@gmail.com>
2023-09-18 16:40:01 -0700

    Hopefully

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_array - print n elements of an array
+* @a: array given
+* @n: number of elements to print
+* Return: void as in none
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == (n - 1))
+			printf("%d\n", a[i]);
+		else
+			printf("%d, ", a[i]);
+	}
+}
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _strcpy - copy string to buffer
+* @dest: the buffer destination
+* @src: the source
+* Return: pointer to the destination
+*/
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+
+	return (dest);
+}
