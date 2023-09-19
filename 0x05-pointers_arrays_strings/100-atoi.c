88113cf419060a9e8c9abed5395e9abefb816642
elishaajayi <ajayienterprises.dev@gmail.com>
2023-09-18 17:33:04 -0700

    Added check for number

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,7 +7,7 @@
  */
 int _atoi(char *s)
 {
-	int i = 0, count = 0, sign = 0;
+	int i = 0, count = 0, sign = 0, check = 0;
 	int j;
 	unsigned int num = 0;
 
@@ -28,9 +28,12 @@ int _atoi(char *s)
 			sign++;
 
 		if (s[j] >= 48 && s[j] <= 57)
+		{
 			num = (num * 10) + (s[j] - '0');
+			check++;
+		}
 
-		if (j > 0 && (s[j] < 48 || s[j] > 57))
+		if (check > 0 && (s[j] < 48 || s[j] > 57))
 			break;
 	}
 
