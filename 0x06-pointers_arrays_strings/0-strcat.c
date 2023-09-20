6d892493a919b9a4bfe7ad86d42213161fb0abb3
elishaajayi <ajayienterprises.dev@gmail.com>
2023-09-20 14:33:10 +0100

    Testing offline setup

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _strcat - function to concatenate two strings
+* @dest: destination string
+* @src: source string
+* Return: pointer to destination
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j;
+
+	while (dest[i] != '\0')
+		i++;
+
+	for (j = 0; src[j] != '\0'; j++)
+		dest[i + j] = src[j];
+
+	return (dest);
+}
