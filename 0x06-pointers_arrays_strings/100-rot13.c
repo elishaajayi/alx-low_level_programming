154f2aab02fe2b9e4ea50a2a5614f54ce41e89ec
elishaajayi <ajayienterprises.dev@gmail.com>
2023-09-21 22:55:23 +0100

    fix 9

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,22 +7,14 @@
 */
 char *rot13(char *str)
 {
-	int i, j;
-	int alp[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A',
-'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
-'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-	int rot[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'N', 'O',
-'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E',
-'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
+	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; j < 52; j++)
+		if ((str[i] >= 'A' && str[i] <= 'Z')
+			|| (str[i] >= 'a' && str[i] <= 'z'))
 		{
-			if (str[i] == alp[j])
-				str[i] = rot[j];
+			str[i] = str[i] + 13;
 		}
 	}
 
