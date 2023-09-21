608f9bd838805ca07ddecfddbbfc5904c8555d29
elishaajayi <ajayienterprises.dev@gmail.com>
2023-09-21 22:42:50 +0100

    101-fix

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,17 +7,19 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
+	unsigned int num = n;
+
+	if (num < 0)
 	{
-		_putchar(45);
-		n = -n;
+		_putchar('-');
+		num = -num;
 	}
 
-	if (n / 10 != 0)
+	if (num / 10 != 0)
 	{
-		print_number(n / 10);
+		print_number(num / 10);
 	}
 
-	_putchar((n % 10) + '0');
+	_putchar((num % 10) + '0');
 }
 
