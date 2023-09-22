9ea2133efbf6de951ec241deb7af92f4ac6bf218
elishaajayi <ajayienterprises.dev@gmail.com>
2023-09-22 02:21:10 +0100

    Advanced complete

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,97 +1,53 @@
 #include "main.h"
-
-/* Prototypes */
-void to_int(char *str, int *num);
-void to_char(int num, char *str);
+#include <wchar.h>
 
 /**
- * infinite_add - function to add two massive numbers
- * @n1: first number
- * @n2: second number
- * @r: result of the sum
- * @size_r: size of r in bytes
- * Return: r as the result to the function call
- */
+* infinite_add - adds two chars in int anc covert to char
+* @n1: first number
+* @n2: second number
+* @r: result os sum in char
+* @size_r: size of r
+* Return: r the added int converted to char string
+*/
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int a = 0, b = 0;
-	int i, num1, num2, sum;
-	char tmp[1000];
+	int i, j, k, l, m, n;
 
-	/* Initializing r with null bytes to prevent error */
-	for (b = 0; b < size_r; b++)
-		r[b] = '\0';
-
-	/* Convert char to int */
-	to_int(n1, &num1);
-	to_int(n2, &num2);
-
-	sum = num1 + num2;
-
-	/* Back to char */
-	to_char(sum, tmp);
-
-	while (tmp[a] != '\0')
-		a++;
-	a++;
-
-	if (a > size_r)
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
 		return (0);
 
-	for (i = 0; i < a; i++)
-		r[i] = tmp[i];
+	m = 0;
+
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+	{
+		n = m;
+		if (i >= 0)
+			n += n1[i] - '0';
+		if (j >= 0)
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
+		{
+			break;
+		}
+		m = n / 10;
+		r[k] = n % 10 + '0';
+	}
+
+	r[k] = '\0';
+
+	if (i >= 0 || j >= 0 || m)
+		return (0);
+
+	for (k -= 1, l = 0; l < k; k--, l++)
+	{
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
+	}
 
 	return (r);
-}
-
-/**
- * to_int - convert char to int
- * @str: char string
- * @num: converted char str
- * Return: void as in none
- */
-void to_int(char *str, int *num)
-{
-	*num = 0;
-
-	while (*str >= '0' && *str <= '9')
-	{
-		*num = *num * 10 + (*str - '0');
-		str++;
-	}
-}
-
-/**
- * to_char - convert char to int
- * @str: char string
- * @num: converted char str
- * Return: void as in none
- */
-void to_char(int num, char *str)
-{
-	int rem = 0, i = 0, j = 0;
-	int last;
-	char temp;
-
-	while (num != 0)
-	{
-		rem = num % 10;
-		str[i] = rem + '0';
-		num = num / 10;
-		i++;
-	}
-
-	/* Reverse char str */
-	last = i - 1;
-
-	while (j < last)
-	{
-		temp = str[j];
-		str[j] = str[last];
-		str[last] = temp;
-		j++;
-		last--;
-	}
-
-	str[i] = '\0';
 }
