a4b077470896b0c7a6c320a9e905a83fcd449689
elishaajayi <ajayienterprises.dev@gmail.com>
2023-09-26 00:40:44 +0100

    Fix

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,22 @@
 #include "main.h"
 
 /**
-* _memset - fill the first n bytes to s with b
-* @s: memory area s
-* @b: char to fill in s
-* @n: the number of bytes to fill
-* Return: s for the meemory area
-*/
+ * _memset - fill the first n bytes to s with b
+ * @s: memory area s
+ * @b: char to fill in s
+ * @n: the number of bytes to fill
+ * Return: s for the meemory area
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	int size = n;
-	int i;
 
-	for (i = 0; i < size; i++)
+	if (size > 0)
 	{
-		s[i] = b;
+		int i;
+
+		for (i = 0; i < size; i++)
+			s[i] = b;
 	}
 
 	return (s);
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,1 +1,2 @@
-Even More Pointers, Arrays and Strings
+Even More Pointers, Arrays and Strings <br>
+This one is causing me a bit more problems
