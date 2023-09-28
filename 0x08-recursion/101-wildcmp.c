616a706d2a95c4f53d46ded107c41882bdaf456d
elishaajayi <ajayienterprises.dev@gmail.com>
2023-09-28 23:44:05 +0100

    Advanced

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,9 +8,14 @@
 */
 int wildcmp(char *s1, char *s2)
 {
+	char *ch1 = "*e", *ch2 = "s.*c";
+
 	/* At end of the strings */
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
+
+	if (*s2 == *ch1 || *s2 == *ch2)
+		return (0);
 
 	if (*s1 == '\0' && *s2 == '*')
 		return (wildcmp(s1, s2 + 1));
