2381652235c9defa4a61edd860b012ab4f22950b
elishaajayi <ajayienterprises.dev@gmail.com>
2023-10-08 21:24:24 +0100

    Task 100: realloc is broken

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
 * _realloc - function to reallocate memory block
@@ -10,10 +11,9 @@
 */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	unsigned int i;
+	/* Apparently, realloc is allowed to return new memory block */
+	void *new_ptr;
 
-	if (new_size > old_size)
-		return (ptr);
 	if (new_size == old_size)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
@@ -21,9 +21,20 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
+	if (ptr == NULL)
+		return (malloc(new_size));
 
-	for (i = new_size; i < old_size; i++)
-		free(ptr + i);
+	if (new_size > old_size || ptr != NULL)
+	{
+		new_ptr = malloc(new_size);
+
+		if (new_ptr == NULL)
+			return (NULL);
+
+		memcpy(new_ptr, ptr, old_size);
+	}
+
+	free(ptr);
 
 	return (ptr);
 }
