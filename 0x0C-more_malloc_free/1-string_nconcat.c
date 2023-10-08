280cdd956111c1dc186cd11d2e10fcb700f62e88
elishaajayi <ajayienterprises.dev@gmail.com>
2023-10-08 08:41:58 +0100

    Task 1 - string concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,15 +1,46 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
-* string_nconcat - function to concatenate two strings
-* @s1: first string
-* @s2: second string
-* @n: the first n bytes of s2 to copy
-* Return: NULL for fail or pointer to concatenated string
-*/
+ * string_nconcat - function to concatenate two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: the first n bytes of s2 to copy
+ * Return: NULL for fail or pointer to concatenated string
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *con_str;
+	unsigned int j;
+	int i, len1 = 0, len2 = 0;
 
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
+
+	con_str = malloc((len1 * sizeof(char)) + (len2 * sizeof(char)) + 1);
+
+	if (con_str == NULL)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+	{
+		if (s1 == NULL)
+		{
+			con_str[i] = ' ';
+			break;
+		}
+		con_str[i] = s1[i];
+	}
+
+	for (j = 0; j < (n / sizeof(char)); j++)
+	{
+		if (s2 == NULL)
+		{
+			con_str[j] = ' ';
+			break;
+		}
+		con_str[i + j] = s2[j];
+	}
 	return (con_str);
 }
