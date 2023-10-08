65dead15dc291289bda9dd5f182d87abf90e233a
elishaajayi <ajayienterprises.dev@gmail.com>
2023-10-08 14:20:15 +0100

    Fixing str_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,37 +10,25 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *con_str;
+	char *s;
 	unsigned int j;
-	int i, len1 = 0, len2 = 0;
+	int i, len, len1 = 0, len2 = 0;
 
 	while (s1[len1] != '\0')
 		len1++;
 	while (s2[len2] != '\0')
 		len2++;
 
-	con_str = malloc((len1 * sizeof(char)) + (len2 * sizeof(char)) + 1);
+	len = len1 + len2;
+	s = malloc(len * sizeof(char) + 1);
 
-	if (con_str == NULL)
+	if (s == NULL)
 		return (NULL);
-	for (i = 0; i < len1; i++)
-	{
-		if (s1 == NULL)
-		{
-			con_str[i] = ' ';
-			break;
-		}
-		con_str[i] = s1[i];
-	}
 
+	for (i = 0; i < len1; i++)
+		s[i] = s1[i];
 	for (j = 0; j < (n / sizeof(char)); j++)
-	{
-		if (s2 == NULL)
-		{
-			con_str[j] = ' ';
-			break;
-		}
-		con_str[i + j] = s2[j];
-	}
-	return (con_str);
+		s[len1 + j] = s2[j];
+
+	return (s);
 }
