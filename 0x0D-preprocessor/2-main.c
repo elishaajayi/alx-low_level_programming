12781af598049bedcf9e529c678d52dcc6bd4689
elishaajayi <ajayienterprises.dev@gmail.com>
2023-10-11 00:31:10 +0100

    Done preprocessing

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - start of runs in the function
+ * Return: 0 for success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef FILE_4_SUM_H
+#define FILE_4_SUM_H
+
+#define SUM(x, y) (x + y)
+
+#endif
