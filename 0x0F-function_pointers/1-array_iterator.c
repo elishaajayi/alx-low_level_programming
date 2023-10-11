43f41896f4c76718e8019b7adfa0660668ae615f
elishaajayi <ajayienterprises.dev@gmail.com>
2023-10-12 00:02:08 +0100

    NULL on task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,6 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	void (*execute)(int) = action;
 	size_t i;
 
-	for (i = 0; i < size; i++)
-		execute(array[i]);
+	if (execute != NULL)
+		for (i = 0; i < size; i++)
+			execute(array[i]);
 }
