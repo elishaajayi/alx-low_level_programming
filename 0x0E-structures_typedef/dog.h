d5d1dbaa5236c1f71fb42659d16b644ec4e76fbe
elishaajayi <ajayienterprises.dev@gmail.com>
2023-10-11 01:15:01 +0100

    declaring typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - simple dog structure
@@ -16,5 +16,10 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+/**
+ * dog_t - new type for struct dog
+ */
+typedef struct dog dog_t;
 
 #endif
