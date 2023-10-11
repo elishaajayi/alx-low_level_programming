f76ca4dabaa9760567476aeb144255e5e7515cc5
elishaajayi <ajayienterprises.dev@gmail.com>
2023-10-12 00:04:20 +0100

    Checking NULL on important stuff

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	void (*execute)(int) = action;
 	size_t i;
 
-	if (size > 0 && execute != NULL)
+	if (array != NULL && execute != NULL)
 		for (i = 0; i < size; i++)
 			execute(array[i]);
 }
