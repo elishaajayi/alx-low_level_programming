200a5123d89d7c733d7b1628e00ef3998191cac4
elishaajayi <ajayienterprises.dev@gmail.com>
2023-10-11 01:08:21 +0100

    Fixed betty description

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
 
+/**
+ * struct dog - simple dog structure
+ * @name: of dog
+ * @age: of dog, float
+ * @owner: who lost their dog
+ */
 struct dog
 {
 	char *name;
