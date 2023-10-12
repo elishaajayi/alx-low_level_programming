4b97b15fc22dca538ab2fc98aa3e50b5e15e9e40
elishaajayi <ajayienterprises.dev@gmail.com>
2023-10-12 02:04:27 +0100

    ANother freaking fix

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -7,10 +7,44 @@
 #include <stddef.h>
 
 /* Prototypes */
+/**
+ *op_add - add
+ *@a: first number
+ *@b: second number
+ *Return: sum
+ */
 int op_add(int a, int b);
+
+/**
+ *op_sub - sub
+ *@a: first number
+ *@b: second number
+ *Return: difference
+ */
 int op_sub(int a, int b);
+
+/**
+ *op_add - multiply
+ *@a: first number
+ *@b: second number
+ *Return: product
+ */
 int op_mul(int a, int b);
+
+/**
+ *op_add - divide
+ *@a: first number
+ *@b: second number
+ *Return: division
+ */
 int op_div(int a, int b);
+
+/**
+ *op_add - modulus
+ *@a: first number
+ *@b: second number
+ *Return: remainder
+ */
 int op_mod(int a, int b);
 
 int (*get_op_func(char *s))(int, int);
