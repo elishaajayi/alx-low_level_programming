26b24ef3a6034408edf694fd3b54c1d0fa9d40a7
elishaajayi <ajayienterprises.dev@gmail.com>
2023-10-12 02:06:51 +0100

    removed stddef

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -4,47 +4,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <stddef.h>
 
 /* Prototypes */
-/**
- *op_add - add
- *@a: first number
- *@b: second number
- *Return: sum
- */
 int op_add(int a, int b);
-
-/**
- *op_sub - sub
- *@a: first number
- *@b: second number
- *Return: difference
- */
 int op_sub(int a, int b);
-
-/**
- *op_add - multiply
- *@a: first number
- *@b: second number
- *Return: product
- */
 int op_mul(int a, int b);
-
-/**
- *op_add - divide
- *@a: first number
- *@b: second number
- *Return: division
- */
 int op_div(int a, int b);
-
-/**
- *op_add - modulus
- *@a: first number
- *@b: second number
- *Return: remainder
- */
 int op_mod(int a, int b);
 
 int (*get_op_func(char *s))(int, int);
@@ -60,6 +25,5 @@ typedef struct op
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
-
 
 #endif
