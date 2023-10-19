948b1bf55091f6b20071e95fe9070b4ecc0ca188
elishaajayi <ajayienterprises.dev@gmail.com>
2023-10-19 18:14:01 +0100

    Advanced 2

---
+++ 0x12-singly_linked_lists/assembly.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - first function to run
+ * Description: Print alphabets without q or e
+ * Return: 0 for success
+ */
+int main(void)
+{
+	printf("Hello Holberton\n");
+	return (0);
+}
