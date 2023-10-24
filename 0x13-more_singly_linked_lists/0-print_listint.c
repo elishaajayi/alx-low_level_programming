81157c2e053ff4a1e2035b63a3bbea0c3d273c18
elishaajayi <ajayienterprises.dev@gmail.com>
2023-10-24 23:59:42 +0100

    another test

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,7 +9,12 @@ size_t print_listint(const listint_t *h)
 {
 	size_t num_nodes = 0;
 
-
+	while (h != NULL)
+	{
+		printf("%i\n", h->n);
+		num_nodes++;
+		h = h->next;
+	}
 
 	return (num_nodes);
 }
--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,42 +7,9 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	int length = 0, index = 0, i = 0, j = 0, check = 0;
-	listint_t const **save_ptr = malloc(sizeof(listint_t) * 10);
+	int length = 0;
 
-	if (!save_ptr)
-		exit(98);
+	printf("%i\n", head->n);
 
-	while (head != NULL)
-	{
-		for (i = 0; i < length; i++)
-		{
-			if (head == save_ptr[i])
-			{
-				check = 1;
-				index = i;
-				break;
-			}
-			else
-				check = 0;
-		}
-
-		if (check == 1)
-			break;
-
-		save_ptr[length] = head;
-		head = head->next;
-		length++;
-	}
-
-	while (j < length)
-	{
-		printf("[%p] %d\n", (void *)save_ptr[j], save_ptr[j]->n);
-		j++;
-	}
-
-	if (check == 1)
-		printf("-> [%p] %d\n", (void *)save_ptr[index], save_ptr[index]->n);
-	free(save_ptr);
 	return (length);
 }
