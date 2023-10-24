8c1c4c35ce09e8391d0ecdbbf7d20b3d84d133ef
elishaajayi <ajayienterprises.dev@gmail.com>
2023-10-24 23:55:57 +0100

    test

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,12 +9,7 @@ size_t print_listint(const listint_t *h)
 {
 	size_t num_nodes = 0;
 
-	while (h != NULL)
-	{
-		printf("%i\n", h->n);
-		num_nodes++;
-		h = h->next;
-	}
+
 
 	return (num_nodes);
 }
