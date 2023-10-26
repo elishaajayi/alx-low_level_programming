38f6e1be6c150f901f683a1c904819404fa02103
elishaajayi <ajayienterprises.dev@gmail.com>
2023-10-26 21:50:27 +0100

    Rewrote task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,12 +8,13 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int check_mask = 1UL << index;
+	unsigned long int check_mask;
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
 
 	if ((n & check_mask) == 0)
 		return (0);
 	else
 		return (1);
-
-	return (-1);
 }
