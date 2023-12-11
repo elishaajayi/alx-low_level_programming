f9834d1c9c1fc0496ad7df205364c38b8e9348da
elishaajayi <ajayienterprises.dev@gmail.com>
2023-12-11 21:12:25 +0100

    Fix: It worked, now to test

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P ./tmp/ https://raw.github.com/elishaajayi/alx-low_level_programming/master/0x18-dynamic_libraries/screwed.so
-export LD_PRELOAD=./tmp/screwed.so
+export LD_PRELOAD=./tmp/screwed.so ./gm 9 8 10 24 75 9
--- 0x18-dynamic_libraries/screwed.c
+++ 0x18-dynamic_libraries/screwed.c
@@ -3,10 +3,23 @@
 
 /**
  * runBeforeMain - as name suggests
+ * @argc: number of arguments
+ * @argv: the arguments recieved
  * Return: void as in none
  */
-void __attribute__((constructor)) runBeforeMain(void)
+void __attribute__((constructor)) runBeforeMain(int argc, char *argv[])
 {
-	printf("--> Please make me win\n");
-	exit(0);
+	if (argc == 7 &&
+			atoi(argv[1]) == 9 &&
+			atoi(argv[2]) == 8 &&
+			atoi(argv[3]) == 10 &&
+			atoi(argv[4]) == 24 &&
+			atoi(argv[5]) == 75 &&
+			atoi(argv[6]) == 9)
+	{
+		printf("--> Please make me win\n");
+		exit(0);
+	}
+	else
+		return;
 }
