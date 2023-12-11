c11951deb94a2b9302f8b42f5eea9623f6df78c9
elishaajayi <ajayienterprises.dev@gmail.com>
2023-12-11 20:35:43 +0100

    Fix: Changing the c file

--- 0x18-dynamic_libraries/screwed.c
+++ 0x18-dynamic_libraries/screwed.c
@@ -2,24 +2,11 @@
 #include <stdlib.h>
 
 /**
- * screwing - the function to inject
- * @argc: the number of inputs
- * @argv: the inputs to consider
+ * runBeforeMain - as name suggests
  * Return: void as in none
  */
-void screwing(int argc, char *argv[])
+void __attribute__((constructor)) runBeforeMain(void)
 {
-	if (argc == 7 &&
-			atoi(argv[1]) == 9 &&
-			atoi(argv[2]) == 8 &&
-			atoi(argv[3]) == 10 &&
-			atoi(argv[4]) == 24 &&
-			atoi(argv[5]) == 75 &&
-			atoi(argv[6]) == 9)
-	{
-		printf("--> Please make me win!\n");
-		exit(0);
-	}
-	else
-		system("./gm 5 5 5 5 5 5");
+	printf("--> Please make me win\n");
+	exit(0);
 }
