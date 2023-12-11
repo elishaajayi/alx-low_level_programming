bd47bb52c24fe6e654fba7e6336986734622b041
elishaajayi <ajayienterprises.dev@gmail.com>
2023-12-11 16:05:38 +0100

    Test: Advanced task, using c for py

---
+++ 0x18-dynamic_libraries/func.c
@@ -0,0 +1,57 @@
+#include "func.h"
+
+/**
+ * add - add two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result of operation
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - add two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result of operation
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - add two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result of operation
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - add two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result of operation
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - add two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result of operation
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
