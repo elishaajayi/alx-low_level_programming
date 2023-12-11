c6ad885ce1b787a7fcdb7b90a963d975ce11d867
elishaajayi <ajayienterprises.dev@gmail.com>
2023-12-11 20:11:55 +0100

    Fix: Radical new change

--- 0x18-dynamic_libraries/screwed.c
+++ 0x18-dynamic_libraries/screwed.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void screwing(void) __attribute__((constructor));
 /**
  * screwing - the function to inject
+ * @argc: the number of inputs
+ * @argv: the inputs to consider
  * Return: void as in none
  */
-void screwing(void)
+void screwing(int argc, char *argv[])
 {
-	printf("--> Please make me win!\n");
+	if (argc == 7 &&
+			atoi(argv[1]) == 9 &&
+			atoi(argv[2]) == 8 &&
+			atoi(argv[3]) == 10 &&
+			atoi(argv[4]) == 24 &&
+			atoi(argv[5]) == 75 &&
+			atoi(argv[6]) == 9)
+	{
+		printf("--> Please make me win!\n");
+		exit(0);
+	}
+	else
+		system("./gm 5 5 5 5 5 5");
 }
