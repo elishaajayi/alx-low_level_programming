f193fee7be846af1847954fe73b6761be4c1d6ab
elishaajayi <ajayienterprises.dev@gmail.com>
2023-12-21 21:23:01 +0100

    $1

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieve a value using a key
+ * @ht: the hash table to look into
+ * @key: the key we are to look for
+ * Return: the value string to llok for
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int i;
+	hash_node_t *current;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	i = key_index((const unsigned char *)key, ht->size);
+	if (i >= ht->size)
+		return (NULL);
+
+	current = ht->array[i];
+	if (current != NULL && strcmp(current->key, key) == 0)
+		return (current->value);
+
+	return (NULL);
+}
